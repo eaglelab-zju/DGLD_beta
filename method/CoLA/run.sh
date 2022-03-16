9c784dd0b09ad053764a87a7952e112a50201938
gavinygm <304753882@qq.com>
2022-03-16 23:34:58 +0800

    fix: ignore logs and fix CoLA run.sh

--- method/CoLA/run.sh
+++ method/CoLA/run.sh
@@ -7,5 +7,5 @@ if [ ! -d log  ];then
 else
   echo dir exist
 fi
-python main.py --dataset $dataset --logdir log/$expname > log/$expname.log
-# nohup python main.py --dataset $dataset --logdir log/$expname > log/$expname.log 2>&1 &
+# python main.py --dataset $dataset --logdir log/$expname > log/$expname.log
+nohup python main.py --dataset $dataset --logdir log/$expname > log/$expname.log 2>&1 &
