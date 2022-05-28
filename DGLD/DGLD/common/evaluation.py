d6184b279d4dafb6b9df57f8fee2696d6d956911
gavinygm <304753882@qq.com>
2022-05-28 16:38:15 +0800

    fix: update dataset to DGLD

--- DGLD/DGLD/common/evaluation.py
+++ DGLD/DGLD/common/evaluation.py
@@ -1,6 +1,7 @@
 from sklearn.metrics import roc_auc_score
 import numpy as np
-def split_auc(groundtruth, prob):
+
+def split_auc(groundtruth, prob,data_name='not_custom'):
     r"""
     print the scoring(AUC) of the two types of anomalies separately.
     Parameter:
@@ -15,25 +16,28 @@ def split_auc(groundtruth, prob):
     -------
     None
     """
-    str_pos_idx = groundtruth == 1
-    attr_pos_idx = groundtruth == 2
-    norm_idx = groundtruth == 0
+    s_score = -1
+    a_score = -1
+    if data_name != 'custom':
+        str_pos_idx = groundtruth == 1
+        attr_pos_idx = groundtruth == 2
+        norm_idx = groundtruth == 0
 
-    str_data_idx = str_pos_idx | norm_idx
-    attr_data_idx = attr_pos_idx | norm_idx
+        str_data_idx = str_pos_idx | norm_idx
+        attr_data_idx = attr_pos_idx | norm_idx
 
-    str_data_groundtruth = groundtruth[str_data_idx]
-    str_data_predict = prob[str_data_idx]
+        str_data_groundtruth = groundtruth[str_data_idx]
+        str_data_predict = prob[str_data_idx]
 
-    attr_data_groundtruth = np.where(groundtruth[attr_data_idx] != 0, 1, 0)
-    attr_data_predict = prob[attr_data_idx]
+        attr_data_groundtruth = np.where(groundtruth[attr_data_idx] != 0, 1, 0)
+        attr_data_predict = prob[attr_data_idx]
 
-    s_score = roc_auc_score(str_data_groundtruth, str_data_predict)
-    a_score = roc_auc_score(attr_data_groundtruth, attr_data_predict)
+        s_score = roc_auc_score(str_data_groundtruth, str_data_predict)
+        a_score = roc_auc_score(attr_data_groundtruth, attr_data_predict)
+        print("structural anomaly score:", s_score)
+        print("attribute anomaly score:", a_score)
+
     final_score = roc_auc_score(np.where(groundtruth == 0, 0, 1), prob)
 
-    print("structural anomaly score:", s_score)
-    print("attribute anomaly score:", a_score)
     print("final anomaly score:", final_score)
     return final_score, a_score, s_score
-
