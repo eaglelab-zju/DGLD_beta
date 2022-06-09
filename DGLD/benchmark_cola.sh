1446546937132f4748321bddb951011326b636a1
gavinygm <304753882@qq.com>
2022-06-09 22:26:16 +0800

    fix(CoLA): update cola script

--- DGLD/benchmark_cola.sh
+++ DGLD/benchmark_cola.sh
@@ -1,7 +1,14 @@
+if [ ! -d log  ];then
+  mkdir log
+  echo mkdir log
+else
+  echo dir exist
+fi
+
 batch=300
-for data in Cora Citeseer Pubmed ogbn-arxiv ACM Flickr Blog
+for data in Cora Citeseer Pubmed ogbn-arxiv ACM Flickr BlogCatalog
 do
-  expname=$data'CoLA'
+  expname=$data'_CoLA'
   dataset=$data
   CUDA_VISIBLE_DEVICES=4 python main_cola.py --dataset $dataset --device 0 --batch_size $batch --logdir log/$expname > log/$expname.log 2>&1 &
 done
