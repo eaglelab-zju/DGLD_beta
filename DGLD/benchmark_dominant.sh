65ff911ce4bf60634fa480ff312859c108503c0f
gavinygm <304753882@qq.com>
2022-06-13 01:05:41 +0800

    fix(DOMINANT): update script and params

--- DGLD/benchmark_dominant.sh
+++ DGLD/benchmark_dominant.sh
@@ -5,12 +5,13 @@ else
   echo log dir exist
 fi
 
+seed=4096
 for data in Cora Citeseer Pubmed ogbn-arxiv ACM Flickr BlogCatalog
 do
   expname=$data'_DOMINANT'
   echo ${expname}
   dataset=$data
-  CUDA_VISIBLE_DEVICES=2 PYTHONHASHSEED=1024 python main_dominant.py --dataset $dataset --device 0 --seed 1024 --logdir log/$expname > log/$expname.log 2>&1
+  CUDA_VISIBLE_DEVICES=2 PYTHONHASHSEED=$seed python main_dominant.py --dataset $dataset --device 0 --seed $seed --logdir log/$expname > log/$expname.log 2>&1
 done
 
 
